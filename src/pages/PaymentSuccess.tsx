408d88e7c0bd79eb6158fcf3890046af50d27489
Elfque <100863540+Elfque@users.noreply.github.com>
2024-05-31 12:28:26 +0100

    go to home button

--- src/pages/PaymentSuccess.tsx
+++ src/pages/PaymentSuccess.tsx
@@ -1,4 +1,5 @@
 import { FiCheckCircle } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 const PaymentSuccess = () => {
   return (
@@ -7,14 +8,16 @@ const PaymentSuccess = () => {
         <div className="flex justify-center">
           <FiCheckCircle className="text-5xl" />
         </div>
-        <div className="successful font-semibold text-2xl mt-6 mb-2">
+        <div className="successful font-semibold text-2xl mt-6 mb-2 ">
           {/* <div className="successful text-green-600 font-semibold text-2xl"> */}
           Payment Successful
         </div>
         <div className="flex justify-center">
-          <button className="text-white bg-black px-6 py-2 rounded-lg font-semibold text-sm">
-            Go to Home
-          </button>
+          <Link to={"/home"}>
+            <button className="text-white bg-black px-6 py-2 rounded-lg font-semibold text-sm">
+              Go to Home
+            </button>
+          </Link>
         </div>
       </div>
     </div>
