42f8827f2ee14292f7ea298eccb0886f9278ccbf
Elfque <100863540+Elfque@users.noreply.github.com>
2024-06-27 16:44:37 +0100

    text

--- src/components/LandingPageNavbar.tsx
+++ src/components/LandingPageNavbar.tsx
@@ -9,7 +9,7 @@ const LandingPageNavbar = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="relative">
+    <div className="relative w-screen">
       <div className="bg-white/80 p-4 relative top-0 z-20">
         <div className="flex justify-between">
           <div className="flex items-center gap-3 relative z-30">
@@ -56,33 +56,35 @@ const LandingPageNavbar = () => {
         </div>
       </div>
 
-      <div
-        className={`absolute md:hidden duration-300 bg-white z-30 top-full p-6 flex flex-col items-center gap-6  ${
-          open ? "right-0" : "-right-60"
-        } `}
-      >
-        <div className="">
-          <div className="flex flex-col gap-6 items-center">
-            <Link to={"/billing"}>
-              <button>Pricing</button>
-            </Link>
+      {open && (
+        <div
+          className={`absolute md:hidden duration-300 bg-white z-30 top-full p-6 flex flex-col items-center gap-6  ${
+            open ? "right-0" : "-right-60"
+          } `}
+        >
+          <div className="">
+            <div className="flex flex-col gap-6 items-center">
+              <Link to={"/billing"}>
+                <button>Pricing</button>
+              </Link>
 
-            {!localStorage.getItem("token") && (
-              <div className="text-center flex flex-col gap-6">
-                <Link to={"/auth/login"}>
-                  <button>Login</button>
-                </Link>
+              {!localStorage.getItem("token") && (
+                <div className="text-center flex flex-col gap-6">
+                  <Link to={"/auth/login"}>
+                    <button>Login</button>
+                  </Link>
 
-                <Link to={"/auth/register"} className="w-full">
-                  <button className="py-2 px-4 rounded-md text-white bg-buttonPurple w-full">
-                    Sign Up
-                  </button>
-                </Link>
-              </div>
-            )}
+                  <Link to={"/auth/register"} className="w-full">
+                    <button className="py-2 px-4 rounded-md text-white bg-buttonPurple w-full">
+                      Sign Up
+                    </button>
+                  </Link>
+                </div>
+              )}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
