057d591bfd037463a52b7d315c51c70d5b44465a
Bruiz <bz@bz.dev>
2024-09-14 22:02:24 +0100

    updatd

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -3,8 +3,12 @@ import Footer from "../components/Footer";
 import LandingPageNavbar from "../components/LandingPageNavbar";
 import SingleFAQ from "../components/SingleFAQ";
 import { openTourVideo } from "../utils/functions";
+import { useState } from "react";
+import VideoTourModal from "../components/VideoTourModal";
 
 const FAQ = () => {
+  const [openVideo, setOpenVideo] = useState(false)
+
   return (
     <div className="faq billing min-h-screen">
       <LandingPageNavbar />
@@ -25,13 +29,15 @@ const FAQ = () => {
             <div>Check out the video below for a full explanation.</div>
             <button
               className="py-2 px-6 rounded-full text-white bg-buttonPurple flex gap-2 items-center mt-2"
-              onClick={openTourVideo}
+              onClick={() => setOpenVideo(v => !v)}
             >
               <FaPlay />
               Watch Tour
             </button>
           </div>
         </SingleFAQ>
+
+        {openVideo && <VideoTourModal close={() => setOpenVideo(v => !v)} />}
 
         <SingleFAQ title="Which languages does Screbbi support?">
           <div>
