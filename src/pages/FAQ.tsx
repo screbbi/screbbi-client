c7e08c4bb92a1b5c52877add74bd6fcaeafd0ae2
Bruiz <bz@bz.dev>
2024-09-14 15:39:22 +0100

    changed:UppercaseTourToLowercaset

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -32,7 +32,7 @@ const FAQ = () => {
               onClick={() => setOpenVideo(v => !v)}
             >
               <FaPlay />
-              Watch Tour
+              Watch tour
             </button>
           </div>
         </SingleFAQ>
